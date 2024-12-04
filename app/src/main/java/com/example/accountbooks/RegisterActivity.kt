6499717821cf6eab8867eff8684cd298f8430a3e
a06246 <a06246@gmail.com>
2024-12-05 04:36:59 +0900

    오류 수정 및 firebase 프로젝트 통일(com.example.accountbooks)

--- app/src/main/java/com/example/accountbooks/RegisterActivity.kt
+++ app/src/main/java/com/example/accountbooks/RegisterActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.sharedbudget.R
+import com.example.accountbooks.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
